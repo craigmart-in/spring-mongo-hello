1c5aab17ccc2c428206b024b89241c95f73ee53d
Craig Martin <CraigM@cci.local>
2019-06-16 19:28:56 -0500

    Move RestTemplate to CommandLineRunner so Spring Boot calls it on startup.

--- src/main/java/com/example/craigmart/spring/Application.java
+++ src/main/java/com/example/craigmart/spring/Application.java
@@ -2,8 +2,11 @@ package com.example.craigmart.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,10 +15,22 @@ public class Application {
     private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
-        //SpringApplication.run(Application.class, args);
+        SpringApplication.run(Application.class, args);
+    }
 
-        RestTemplate restTemplate = new RestTemplate();
-        Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
-        log.info(quote.toString());
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+
+    @Bean
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            Quote quote = restTemplate.getForObject(
+                    "https://gturnquist-quoters.cfapps.io/api/random",
+                    Quote.class);
+
+            log.info(quote.toString());
+        };
     }
 }
